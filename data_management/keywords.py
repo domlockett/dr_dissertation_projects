9da4e7db364941e687193cf687265745a7020e20
Dominique L <34702780+domlockett@users.noreply.github.com>
2024-08-03 14:40:05 -0500

    Create keywords.py

---
+++ data_management/keywords.py
@@ -0,0 +1,706 @@
+[
+  {
+    "category": "things",
+    "words": [
+        {
+            "keyword": "Advertisement",
+            "alts": []
+        },
+        {
+            "keyword": "Agave",
+            "alts": []
+        },
+        {
+            "keyword": "Air compressor",
+            "alts": []
+        },
+        {
+            "keyword": "Air Conditioner",
+            "alts": [
+                "Air Conditioning"
+            ]
+        },
+        {
+            "keyword": "Air filter",
+            "alts": []
+        },
+        {
+            "keyword": "Air vent",
+            "alts": []
+        },
+        {
+            "keyword": "Alarm system",
+            "alts": []
+        },
+        {
+            "keyword": "Analogy",
+            "alts": []
+        },
+        {
+            "keyword": "Anemone",
+            "alts": []
+        },
+        {
+            "keyword": "Anesthesia",
+            "alts": []
+        },
+        {
+            "keyword": "Apple pie",
+            "alts": []
+        },
+        {
+            "keyword": "Aprons",
+            "alts": []
+        },
+        {
+            "keyword": "Aquarium",
+            "alts": []
+        },
+        {
+            "keyword": "Atmosphere",
+            "alts": []
+        },
+        {
+            "keyword": "Auditorium",
+            "alts": []
+        },
+        {
+            "keyword": "Backrest",
+            "alts": []
+        },
+        {
+            "keyword": "Bacteria",
+            "alts": []
+        },
+        {
+            "keyword": "Baguette",
+            "alts": []
+        },
+        {
+            "keyword": "Balcony",
+            "alts": []
+        },
+        {
+            "keyword": "Bank",
+            "alts": []
+        },
+        {
+            "keyword": "Barber Chair",
+            "alts": []
+        },
+        {
+            "keyword": "Barcode",
+            "alts": []
+        },
+        {
+            "keyword": "Bat",
+            "alts": []
+        },
+        {
+            "keyword": "Bath Mat",
+            "alts": []
+        },
+        {
+            "keyword": "Battery Bank",
+            "alts": []
+        },
+        {
+            "keyword": "Beanbag",
+            "alts": []
+        },
+        {
+            "keyword": "Bed frame",
+            "alts": []
+        },
+        {
+            "keyword": "Bike",
+            "alts": []
+        },
+        {
+            "keyword": "Bike path",
+            "alts": [
+                "Bike trail"
+            ]
+        },
+        {
+            "keyword": "Binoculars",
+            "alts": []
+        },
+        {
+            "keyword": "Bird cage",
+            "alts": []
+        },
+        {
+            "keyword": "Bird seed",
+            "alts": []
+        },
+        {
+            "keyword": "Bleachers",
+            "alts": []
+        },
+        {
+            "keyword": "Blinds",
+            "alts": []
+        },
+        {
+            "keyword": "Board Games",
+            "alts": []
+        },
+        {
+            "keyword": "Bobby Pins",
+            "alts": []
+        },
+        {
+            "keyword": "Bollards",
+            "alts": []
+        },
+        {
+            "keyword": "Bonding Agent",
+            "alts": []
+        },
+        {
+            "keyword": "Bookcase",
+            "alts": []
+        },
+        {
+            "keyword": "Bookends",
+            "alts": []
+        },
+        {
+            "keyword": "Bottled Water",
+            "alts": []
+        },
+        {
+            "keyword": "Bread knife",
+            "alts": []
+        },
+        {
+            "keyword": "Bread pudding",
+            "alts": []
+        },
+        {
+            "keyword": "Break room",
+            "alts": []
+        },
+        {
+            "keyword": "Brewery merchandise",
+            "alts": []
+        },
+        {
+            "keyword": "Briefcase",
+            "alts": []
+        },
+        {
+            "keyword": "Brochure",
+            "alts": []
+        },
+        {
+            "keyword": "Broken glass",
+            "alts": []
+        },
+        {
+            "keyword": "Brownies",
+            "alts": []
+        },
+        {
+            "keyword": "Bug spray",
+            "alts": []
+        },
+        {
+            "keyword": "Bulb",
+            "alts": []
+        },
+        {
+            "keyword": "Bumper sticker",
+            "alts": []
+        },
+        {
+            "keyword": "Bunsen burner",
+            "alts": []
+        },
+        {
+            "keyword": "Butterfly",
+            "alts": [
+                "Butterflies"
+            ]
+        },
+        {
+            "keyword": "Cabinet",
+            "alts": []
+        },
+        {
+            "keyword": "Calculator",
+            "alts": []
+        },
+        {
+            "keyword": "Cantaloupe",
+            "alts": []
+        },
+        {
+            "keyword": "Car Seat",
+            "alts": []
+        },
+        {
+            "keyword": "Card",
+            "alts": []
+        },
+        {
+            "keyword": "Cardboard box",
+            "alts": [
+                "Cardboard boxes"
+            ]
+        },
+        {
+            "keyword": "Cash Register",
+            "alts": []
+        },
+        {
+            "keyword": "Cat bed",
+            "alts": []
+        },
+        {
+            "keyword": "Cat carrier",
+            "alts": []
+        },
+        {
+            "keyword": "Cauliflower",
+            "alts": []
+        },
+        {
+            "keyword": "Ceiling fan",
+            "alts": [
+                "fan"
+            ]
+        },
+        {
+            "keyword": "Cereal",
+            "alts": []
+        },
+        {
+            "keyword": "Latte",
+            "alts": []
+        },
+        {
+            "keyword": "Champagne flute",
+            "alts": []
+        },
+        {
+            "keyword": "Chandelier",
+            "alts": []
+        },
+        {
+            "keyword": "Cheesecake",
+            "alts": []
+        },
+        {
+            "keyword": "Chessboard",
+            "alts": []
+        },
+        {
+            "keyword": "Chew toy",
+            "alts": []
+        },
+        {
+            "keyword": "Chocolate cake",
+            "alts": []
+        },
+        {
+            "keyword": "Cinnamon roll",
+            "alts": []
+        },
+        {
+            "keyword": "Rags",
+            "alts": []
+        },
+        {
+            "keyword": "Coat Rack",
+            "alts": []
+        },
+        {
+            "keyword": "Coffee beans",
+            "alts": []
+        },
+        {
+            "keyword": "Coffee Grinder",
+            "alts": []
+        },
+        {
+            "keyword": "Coffee grounds",
+            "alts": []
+        },
+        {
+            "keyword": "Coffee Makers",
+            "alts": [
+                "Coffee Machine",
+                "Coffee machines"
+            ]
+        },
+        {
+            "keyword": "Comic Book",
+            "alts": [
+                "Comics",
+                "Comic"
+            ]
+        },
+        {
+            "keyword": "Contact lenses",
+            "alts": []
+        },
+        {
+            "keyword": "Conveyor belt",
+            "alts": []
+        },
+        {
+            "keyword": "Cooling Tower",
+            "alts": []
+        },
+        {
+            "keyword": "Coral reefs",
+            "alts": []
+        },
+        {
+            "keyword": "Cream cheese",
+            "alts": []
+        },
+        {
+            "keyword": "Crochet Hook",
+            "alts": []
+        },
+        {
+            "keyword": "Croissant",
+            "alts": []
+        },
+        {
+            "keyword": "Cup holder",
+            "alts": []
+        },
+        {
+            "keyword": "Cupcake",
+            "alts": []
+        },
+        {
+            "keyword": "Curling Iron",
+            "alts": []
+        },
+        {
+            "keyword": "Curtains",
+            "alts": []
+        },
+        {
+            "keyword": "Cutlery",
+            "alts": []
+        },
+        {
+            "keyword": "Cutting Board",
+            "alts": []
+        },
+        {
+            "keyword": "Dandelion",
+            "alts": []
+        },
+        {
+            "keyword": "Deciduous tree",
+            "alts": []
+        },
+        {
+            "keyword": "Dental Chair",
+            "alts": []
+        },
+        {
+            "keyword": "Desk Chairs",
+            "alts": []
+        },
+        {
+            "keyword": "Desk lamp",
+            "alts": []
+        },
+        {
+            "keyword": "Desktop computer",
+            "alts": []
+        },
+        {
+            "keyword": "Diaper",
+            "alts": []
+        },
+        {
+            "keyword": "Dijon mustard",
+            "alts": []
+        },
+        {
+            "keyword": "Dining table",
+            "alts": []
+        },
+        {
+            "keyword": "Dish Rack",
+            "alts": []
+        },
+        {
+            "keyword": "Dish soap",
+            "alts": []
+        },
+        {
+            "keyword": "Disinfectant",
+            "alts": []
+        },
+        {
+            "keyword": "Diving board",
+            "alts": [
+                "Diving platform"
+            ]
+        },
+        {
+            "keyword": "Dog bed",
+            "alts": []
+        },
+        {
+            "keyword": "Dog crate",
+            "alts": []
+        },
+        {
+            "keyword": "Dog shampoo",
+            "alts": []
+        },
+        {
+            "keyword": "Donuts",
+            "alts": []
+        },
+        {
+            "keyword": "Drain Hose",
+            "alts": []
+        },
+        {
+            "keyword": "Drapes",
+            "alts": []
+        },
+        {
+            "keyword": "Duct tape",
+            "alts": []
+        },
+        {
+            "keyword": "Duffle Bags",
+            "alts": []
+        },
+        {
+            "keyword": "Dumbbells",
+            "alts": [
+                "Dumbbell Weights"
+            ]
+        },
+        {
+            "keyword": "Dump truck",
+            "alts": []
+        },
+        {
+            "keyword": "Duvet",
+            "alts": []
+        },
+        {
+            "keyword": "DVD",
+            "alts": []
+        },
+        {
+            "keyword": "DVD Player",
+            "alts": []
+        },
+        {
+            "keyword": "Dynamite",
+            "alts": []
+        },
+        {
+            "keyword": "Ear protection",
+            "alts": []
+        },
+        {
+            "keyword": "Earl Grey tea",
+            "alts": []
+        },
+        {
+            "keyword": "Earplug",
+            "alts": []
+        },
+        {
+            "keyword": "Earth",
+            "alts": []
+        },
+        {
+            "keyword": "Edamame",
+            "alts": []
+        },
+        {
+            "keyword": "Edible flowers",
+            "alts": []
+        },
+        {
+            "keyword": "Electric scooter",
+            "alts": []
+        },
+        {
+            "keyword": "Electric toothbrush",
+            "alts": []
+        },
+        {
+            "keyword": "Electrical outlet",
+            "alts": []
+        },
+        {
+            "keyword": "Electrical panel",
+            "alts": []
+        },
+        {
+            "keyword": "Electrical tape",
+            "alts": []
+        },
+        {
+            "keyword": "Elevator",
+            "alts": []
+        },
+        {
+            "keyword": "Elliptical Trainers",
+            "alts": []
+        },
+        {
+            "keyword": "Emergency Exit Sign",
+            "alts": []
+        },
+        {
+            "keyword": "Emergency lights",
+            "alts": []
+        },
+        {
+            "keyword": "Energy drink",
+            "alts": []
+        },
+        {
+            "keyword": "Engravings",
+            "alts": []
+        },
+        {
+            "keyword": "Escalators",
+            "alts": []
+        },
+        {
+            "keyword": "Eucalyptus",
+            "alts": []
+        },
+        {
+            "keyword": "Excavator",
+            "alts": []
+        },
+        {
+            "keyword": "Exercise Mat",
+            "alts": []
+        },
+        {
+            "keyword": "Exhaust Fan",
+            "alts": []
+        },
+        {
+            "keyword": "Exit sign",
+            "alts": []
+        },
+        {
+            "keyword": "Extension Cord",
+            "alts": []
+        },
+        {
+            "keyword": "Eye mask",
+            "alts": []
+        },
+        {
+            "keyword": "Face mask",
+            "alts": [
+                "Facial Mask"
+            ]
+        },
+        {
+            "keyword": "Facial Toner",
+            "alts": []
+        },
+        {
+            "keyword": "Fanny pack",
+            "alts": []
+        },
+        {
+            "keyword": "Fertilizer",
+            "alts": []
+        },
+        {
+            "keyword": "Filing cabinet",
+            "alts": []
+        },
+        {
+            "keyword": "Finger food",
+            "alts": []
+        },
+        {
+            "keyword": "Fire alarm",
+            "alts": []
+        },
+        {
+            "keyword": "Fire Escape Ladder",
+            "alts": []
+        },
+        {
+            "keyword": "Fire Extinguisher",
+            "alts": []
+        },
+        {
+            "keyword": "Fireplace",
+            "alts": []
+        },
+        {
+            "keyword": "First Aid Kit",
+            "alts": []
+        },
+        {
+            "keyword": "Fish tank",
+            "alts": []
+        },
+        {
+            "keyword": "Fishing pier",
+            "alts": [
+                "pier"
+            ]
+        },
+        {
+            "keyword": "Fitness tracker",
+            "alts": []
+        },
+        {
+            "keyword": "Flashlight",
+            "alts": []
+        },
+        {
+            "keyword": "Floor jacks",
+            "alts": []
+        },
+        {
+            "keyword": "Floor Mats",
+            "alts": []
+        },
+        {
+            "keyword": "Foam roller",
+            "alts": []
+        },
+        {
+            "keyword": "Fog machine",
+            "alts": []
+        },
+        {
+            "keyword": "Food bowl",
+            "alts": []
+        },
+        {
+            "keyword": "Food warmers",
+            "alts": []
+        },
+        {
+            "keyword": "Fortune cookie",
+            "alts": []
+        },
+        {
+            "keyword": "Frappuccino",
+            "alts": []
+        },
+        {
+            "keyword":
